669d31fdf150457b7bb870f3b9d93e3a22388d9e
tranhuutuan261103 <tranhuutuan26112003@gmail.com>
2024-01-15 23:17:52 +0700

    Import 'Text editor'

--- src/views/CreatePost/CreatePost.jsx
+++ src/views/CreatePost/CreatePost.jsx
@@ -1,10 +1,74 @@
 import React from "react";
+import ReactQuill from 'react-quill';
 import './CreatePost.css';
+import 'react-quill/dist/quill.snow.css';
+import { FollowLecture } from "../containers";
+
+const modules = {
+  toolbar: [
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    [{ 'font': [] }],
+    [{ 'size': [] }],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [
+      { 'list': 'ordered' },
+      { 'list': 'bullet' },
+      { 'indent': '-1' },
+      { 'indent': '+1' },
+    ],
+    ['link', 'image', 'video'],
+  ],
+}
+
+const lectures = [
+    {
+        id: 1,
+        name: "Chiến thắng Điện Biên Phủ 1954"
+    },
+    {
+        id: 2,
+        name: "Lecture 2"
+    },
+    {
+        id: 3,
+        name: "Lecture 3"
+    }
+];
 
 const CreatePost = () => {
     return (
         <div className="createPost">
-            <h1>Create Post page...</h1>
+            <div className="createPost-left">
+                <div className="createPost-left__header">
+                    <h1 className="createPost-left__header-heading">Create a post</h1>
+                </div>
+                <div className="createPost-left__body">
+                    <div className="createPost-left__body-title">
+                        <input type="text" placeholder="Title" />
+                    </div>
+                    <div className="createPost-left__body-content">
+                        <ReactQuill 
+                            theme="snow"
+                            modules={modules}
+                            className="createPost-left__body-content-editor"
+                            placeholder="Content"
+                         />
+                    </div>
+                    <div className="createPost-left__body-footer">
+                        <button className="createPost-left__body-footer-btn">Post</button>
+                    </div>
+                </div>
+            </div>
+            <div className="createPost-right">
+                <select className="createPost-right__select">
+                    {
+                        lectures.map((lecture) => (
+                            <option key={lecture.id} value={lecture.id} className="createPost-right__option">{lecture.name}</option>
+                        ))
+                    }
+                </select>
+                <FollowLecture />
+            </div>
         </div>
     );
 }
